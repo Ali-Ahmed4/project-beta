751f4c550e68d7ad7ebd53b86bef8c13c7958287
Ali-Ahmed4 <ali.aviz111@gmail.com>
2023-06-19 00:25:48 +0500

    minor app.js change #5

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/hello", (req, res) => res.send('hello world'));
+app.get('/hello', (req, res) => res.send('hello world'));
 
 /* app.listen(port, () => {
 	console.log("App running on port: ", port);
