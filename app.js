b5dc4b71fb10748f590c40905c5be884d8db81a8
Ali-Ahmed4 <ali.aviz111@gmail.com>
2023-06-18 21:02:24 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const Serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+	res.send("Hello World");
+});
+
+app.listen(port, () => {
+	console.log("App running on port: ", port);
+});
+
+module.exports.handler = Serverless(app);
