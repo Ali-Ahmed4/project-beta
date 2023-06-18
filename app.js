aad0baedd5883d2e9e482e94d5a70830632d72dd
Ali-Ahmed4 <ali.aviz111@gmail.com>
2023-06-19 00:41:54 +0500

    minor app.js change #6

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3000; 
 
 app.get('/hello', (req, res) => res.send('hello world'));
 
