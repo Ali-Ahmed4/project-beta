d6730f3b2acf33ffbe50f91e68c699d65db3cc24
Ali-Ahmed4 <ali.aviz111@gmail.com>
2023-06-18 23:57:17 +0500

    minor app.js change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/hello", (req, res) => {
+app.get("/", (req, res) => {
 	res.status(200).json({
 		message: "success",
 		data: "hello world"
