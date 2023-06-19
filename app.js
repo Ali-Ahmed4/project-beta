bf234b83ec26804fe32e1c273b6fce6b8c878848
Ali-Ahmed4 <ali.aviz111@gmail.com>
2023-06-19 16:18:23 +0500

    implementating socket

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 const serverless = require("serverless-http");
 const express = require("express");
+const path = require("path");
+const http = require("http");
 const app = express();
-const port = 3000; 
+const socketio = require("socket.io");
+const port = 3000 || process.env.PORT;
 
-app.get('/hello', (req, res) => res.send('hello world #1'));
+/* for importing env variable */
+require("dotenv").config();
 
-/* app.listen(port, () => {
-	console.log("App running on port: ", port);
+const server = http.createServer(app);
+const io = socketio(server);
+
+/* Run when client connects */
+io.on("connection", (socket) => {
+
+
+	/* to single client that just connected(current user) */
+	socket.emit("message", "Welcome to chatCord!");
+
+	/* to everyone in the groupchat except the joined client */
+	socket.broadcast.emit("message", "A user has joined the chat");
+
+	socket.on("disconnect", () => {
+		io.emit("message", "A user has left the chat");
+	});
 });
- */
-module.exports.handler = serverless(app);
+
+/* set static folder */
+app.use(express.static(path.join(__dirname, "public")));
+
+/* Api route */
+app.get("/hello", (req, res) => res.send("hello world #1"));
+
+/* Connection to server */
+if (process.env.ENV != "local") {
+	module.exports.handler = serverless(app);
+} else {
+	server.listen(port, () => {
+		console.log("App running on port: ", port);
+	});
+}
